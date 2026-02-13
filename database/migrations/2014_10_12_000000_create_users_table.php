b5cb47fef601eb225ea70a7f63779e2033a2c718
pyo_awndi <154031276+piyo12052004@users.noreply.github.com>
2026-02-13 15:04:44 +0700

    update tabel users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
-            $table->text('liked_films')->nullable();
             // $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
